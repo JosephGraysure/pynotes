81fa277e7c4b3e22deb7acb46ec05385271edcc7
JosephGraysure <32880293+JosephGraysure@users.noreply.github.com>
2017-10-17 13:03:49 -0500

    Update int_or_float.py

--- Exercises/int_or_float.py
+++ Exercises/int_or_float.py
@@ -9,32 +9,24 @@ The other checks if an input string is a float.
 """
 
 def is_int(n):
-    """ Return True if n is an integer.
-
-    >>> is_int('3.0')
-    >>> True
-    >>> is_int('3.002')
-    >>> False
-    """
+    if (n%1 != 0):
+        return false
+    else:
+        return true
 
     # your code here
 
     
 def is_float(n):
-    """ Return True if n is a float.
-
-    >>> is_float('3.0')
-    >>> False
-    >>> is_float('3.002')
-    >>> True
-    """
-
-    # your code here
+    if (n%1 == 0):
+        return false
+    else:
+        return true
 
 
 # Testing the functions
 
-"""
+
 nums = ['12', '12.3', '12.0', '123.002']
 
 for num in nums:
@@ -42,7 +34,7 @@ for num in nums:
         print("{} can be safely converted to an integer.".format(num))
     elif is_float(num):
         print("{} is a float with non-zero digit(s) in the fractional-part.".format(num))
-"""
+
 
 # When your functions are ready, run the above loop. It should print:
 
